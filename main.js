736e881bf5b7ce7e8a62272e9a061836cb830c26
Robin <35682065+war0@users.noreply.github.com>
2022-08-22 16:31:00 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         FarmRPG - Extension by Waro
+// @version      0.1
+// @description  An extension for the game 'Farm RPG' which adds a lot of different features 
+// @author       Waro
+// @match        https://farmrpg.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=farmrpg.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    console.log('Starting FRPG extension...');
+    
+    // some magic will happen here ...
+    
+    console.log('Extension fully loaded!');
+})();
